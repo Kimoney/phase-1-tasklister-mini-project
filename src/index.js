f5e943a14a82c8bfbc927d80bfc63761038dda4e
John <johntaykimani@gmail.com>
2024-01-06 12:41:14 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("create-task-form");
+  form.addEventListener('submit', 
+      function (myEvent){
+        myEvent.preventDefault();
+
+        let li = document.createElement("li")
+        li.textContent = myEvent.target.new_task_description.value
+        
+        document.getElementById("tasks").appendChild(li)
+
+      }
+  )
+  
 });
